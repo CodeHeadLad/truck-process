3a86ef1261dae7b8972a3f19c5e9c65381ff4857
CodeHeadLad <118319421+CodeHeadLad@users.noreply.github.com>
2023-01-25 16:03:06 +0000

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -28,10 +28,6 @@ basic.forever(function () {
     pins.analogWritePin(AnalogPin.P1, 1023)
     reading = pins.analogReadPin(AnalogPin.P0)
     pins.analogWritePin(AnalogPin.P1, 0)
-    led.plotBarGraph(
-    reading,
-    1023
-    )
     if (input.buttonIsPressed(Button.A)) {
         basic.showNumber(reading)
     }
