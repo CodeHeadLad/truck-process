83f87026f54c49bffcb70fe0c703e0ee4a81fa24
CodeHeadLad <118319421+CodeHeadLad@users.noreply.github.com>
2023-01-29 11:15:11 +0000

    Update pxt.json, main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -1,37 +1,49 @@
-def on_received_number(receivedNumber):
-    if receivedNumber == 1:
-        Kitronik_Robotics_Board.motor_on(Kitronik_Robotics_Board.Motors.MOTOR1,
-            Kitronik_Robotics_Board.MotorDirection.FORWARD,
-            85)
+def on_received_string(receivedString):
+    if receivedString == "forward":
         Kitronik_Robotics_Board.motor_on(Kitronik_Robotics_Board.Motors.MOTOR2,
             Kitronik_Robotics_Board.MotorDirection.FORWARD,
             46)
-        basic.show_leds("""
-            . . # . .
-                        . # # # .
-                        . . # . .
-                        . . # . .
-                        . . # . .
-        """)
-    if receivedNumber == 2:
-        Kitronik_Robotics_Board.motor_on(Kitronik_Robotics_Board.Motors.MOTOR1,
-            Kitronik_Robotics_Board.MotorDirection.REVERSE,
-            85)
+        basic.show_arrow(ArrowNames.NORTH)
+    if receivedString == "reverse":
         Kitronik_Robotics_Board.motor_on(Kitronik_Robotics_Board.Motors.MOTOR2,
-            Kitronik_Robotics_Board.MotorDirection.REVERSE,
+            Kitronik_Robotics_Board.MotorDirection.FORWARD,
             46)
+        basic.show_arrow(ArrowNames.SOUTH)
+    if receivedString == "right":
+        Kitronik_Robotics_Board.servo_write(Kitronik_Robotics_Board.Servos.SERVO1, 135)
+        basic.show_arrow(ArrowNames.EAST)
+    if receivedString == "left":
         Kitronik_Robotics_Board.servo_write(Kitronik_Robotics_Board.Servos.SERVO1, 45)
-        basic.show_icon(IconNames.SWORD)
-radio.on_received_number(on_received_number)
+        basic.show_arrow(ArrowNames.WEST)
+    if receivedString == "centetered":
+        Kitronik_Robotics_Board.servo_write(Kitronik_Robotics_Board.Servos.SERVO1, 90)
+        basic.show_arrow(ArrowNames.NORTH)
+    radio.send_number(radio.received_packet(RadioPacketProperty.SIGNAL_STRENGTH))
+radio.on_received_string(on_received_string)
 
 def on_button_pressed_b():
     Kitronik_Robotics_Board.all_off()
 input.on_button_pressed(Button.B, on_button_pressed_b)
 
+# https://makecode.microbit.org/projects/soil-moisture/code
+reading = 0
 radio.set_group(1)
 Kitronik_Robotics_Board.servo_write(Kitronik_Robotics_Board.Servos.SERVO1, 0)
 basic.show_icon(IconNames.YES)
+radio.set_transmit_power(7)
 
 def on_forever():
-    pass
+    global reading
+    pins.analog_write_pin(AnalogPin.P1, 1023)
+    reading = pins.analog_read_pin(AnalogPin.P0)
+    pins.analog_write_pin(AnalogPin.P1, 0)
+    if input.button_is_pressed(Button.A):
+        basic.show_number(reading)
+    if reading < 500:
+        Kitronik_Robotics_Board.motor_on(Kitronik_Robotics_Board.Motors.MOTOR1,
+            Kitronik_Robotics_Board.MotorDirection.FORWARD,
+            46)
+    else:
+        Kitronik_Robotics_Board.motor_off(Kitronik_Robotics_Board.Motors.MOTOR1)
+    basic.pause(5000)
 basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -1,44 +1,54 @@
-radio.onReceivedString(function (receivedString) {
+radio.onReceivedString(function on_received_string(receivedString: string) {
     if (receivedString == "forward") {
         Kitronik_Robotics_Board.motorOn(Kitronik_Robotics_Board.Motors.Motor2, Kitronik_Robotics_Board.MotorDirection.Forward, 46)
         basic.showArrow(ArrowNames.North)
     }
+    
     if (receivedString == "reverse") {
         Kitronik_Robotics_Board.motorOn(Kitronik_Robotics_Board.Motors.Motor2, Kitronik_Robotics_Board.MotorDirection.Forward, 46)
         basic.showArrow(ArrowNames.South)
     }
+    
     if (receivedString == "right") {
         Kitronik_Robotics_Board.servoWrite(Kitronik_Robotics_Board.Servos.Servo1, 135)
         basic.showArrow(ArrowNames.East)
     }
+    
     if (receivedString == "left") {
         Kitronik_Robotics_Board.servoWrite(Kitronik_Robotics_Board.Servos.Servo1, 45)
         basic.showArrow(ArrowNames.West)
     }
+    
     if (receivedString == "centetered") {
         Kitronik_Robotics_Board.servoWrite(Kitronik_Robotics_Board.Servos.Servo1, 90)
         basic.showArrow(ArrowNames.North)
     }
+    
+    radio.sendNumber(radio.receivedPacket(RadioPacketProperty.SignalStrength))
 })
-input.onButtonPressed(Button.B, function () {
+input.onButtonPressed(Button.B, function on_button_pressed_b() {
     Kitronik_Robotics_Board.allOff()
 })
-// https://makecode.microbit.org/projects/soil-moisture/code
+//  https://makecode.microbit.org/projects/soil-moisture/code
 let reading = 0
 radio.setGroup(1)
 Kitronik_Robotics_Board.servoWrite(Kitronik_Robotics_Board.Servos.Servo1, 0)
 basic.showIcon(IconNames.Yes)
-basic.forever(function () {
+radio.setTransmitPower(7)
+basic.forever(function on_forever() {
+    
     pins.analogWritePin(AnalogPin.P1, 1023)
     reading = pins.analogReadPin(AnalogPin.P0)
     pins.analogWritePin(AnalogPin.P1, 0)
     if (input.buttonIsPressed(Button.A)) {
         basic.showNumber(reading)
     }
+    
     if (reading < 500) {
         Kitronik_Robotics_Board.motorOn(Kitronik_Robotics_Board.Motors.Motor1, Kitronik_Robotics_Board.MotorDirection.Forward, 46)
     } else {
         Kitronik_Robotics_Board.motorOff(Kitronik_Robotics_Board.Motors.Motor1)
     }
+    
     basic.pause(5000)
 })
